2a7f4831ba222c50f42b0e78ce377c9a62bfb1cf
JillisDeBoom <Jillis123@hotmail.nl>
2021-03-11 20:27:27 +0100

    PublicPersonName&Age

---
+++ src/nl/novi/Main.java
@@ -0,0 +1,12 @@
+package nl.novi;
+
+import javax.naming.Name;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person student = new Person("jillis","de Boom", 24);
+
+        student.ageOneYear();
+    }
+}
